930e4f6e99e89c28ac8715dd3d2ef78a73a31548
Кирилл <kirillvolk@MacBook-Air-Kirill.local>
2025-03-08 21:58:50 +0300

    first step

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id ("com.github.ben-manes.versions") version "0.52.0"
+    application
+    checkstyle
+    jacoco
+}
+
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+application {
+    mainClass.set("hexlet.code.App")
+}
